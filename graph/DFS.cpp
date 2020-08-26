cfeec1c9195c68f8be529b6e23836bacaac4fa76
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-08-26 11:22:35 +0530

    recursive DFS

--- graph/DFS.cpp
+++ graph/DFS.cpp
@@ -7,15 +7,18 @@ using namespace std;
 class Graph {
     int nodes;
     vector<vector<int> > adjList;
+
     public:
     Graph(int n) {
         this->nodes = n;
         adjList.resize(n);
     }
+
     void addNode(int v, int u) {
         adjList[u].push_back(v);
         adjList[v].push_back(u);
     }
+
     bool DFS(int source, int destination) {
         stack<int> s;
         vector<bool> visited(nodes, false);
@@ -32,6 +35,18 @@ class Graph {
         }
         return false;
     }
+
+    bool DFSRecursive(int a, int b, vector<bool>visited) {
+        if(a == b) return true;
+        visited[a] = true;
+        for(int i = 0; i < adjList[a].size(); i++) {
+            int node = adjList[a][i];
+            if(!visited[node]) {
+                if(DFSRecursive(node, b, visited)) return true;
+            }
+        }
+        return false;
+    }
 };
 
 int main() {
@@ -40,5 +55,5 @@ int main() {
     g.addNode(1, 3);
     g.addNode(0, 2);
     g.addNode(2, 4);
-    cout << g.DFS(1, 4) << endl;
+    cout << g.DFSRecursive(1, 4, vector<bool>(5, false)) << endl;
 }
